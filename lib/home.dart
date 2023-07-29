707d0ee52a5994ca051e17966176276aa3461af8
KanishkaMK <kanishkasudhakaran@gmail.com>
2023-07-29 10:39:06 +0530

    1st commit

---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text('data'),
+          Text('data'),
+          ElevatedButton(onPressed: () {
+            
+          }, child: Text('press'),
+          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.black)),)
+        ],
+      ),
+      
+    );
+  }
+}
